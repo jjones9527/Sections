3886086b3f05f79db55ecbbf605123f63630ada9
Jeremy <jjones9527@gmail.com>
2013-12-02 21:01:28 -0500

    Initial Commit

---
+++ Sections/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sections
+//
+//  Created by Jeremy Jones on 12/2/2013.
+//  Copyright (c) 2013 Jeremy Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sections/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sections
+//
+//  Created by Jeremy Jones on 12/2/2013.
+//  Copyright (c) 2013 Jeremy Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
